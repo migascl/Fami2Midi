15ac09318c675c4ea01914071795b0ee5508b8f2
Miguel Leirosa <miguelleirosa@gmail.com>
2024-09-20 11:02:08 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Fami2Midi;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string content = ReadFile(args[0]);
+    }
+
+    static string ReadFile(string path)
+    {
+        FileInfo file = new FileInfo(path);
+        if (!file.Exists) throw new FileNotFoundException();
+        if (file.Extension != ".json") throw new FormatException("Not a JSON file.");
+        return File.ReadAllText(path);
+    }
+}
