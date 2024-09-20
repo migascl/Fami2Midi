b3f15b7d31ee9541b8b6f391c01ddc4cdc9864b4
Miguel Leirosa <miguelleirosa@gmail.com>
2024-09-20 11:45:08 +0100

    Added FamiTracker Metadata

---
+++ FamiTracker/Metadata.cs
@@ -0,0 +1,9 @@
+﻿namespace FamiTracker;
+
+public class Metadata
+{
+    public string Artist { get; set; } = String.Empty;
+    public string Comment { get; set; } = String.Empty;
+    public string Copyright { get; set; } = String.Empty;
+    public string Title { get; set; } = String.Empty;
+}
--- FamiTracker/Project.cs
+++ FamiTracker/Project.cs
@@ -3,4 +3,5 @@
 public class Project
 {
     public static Globals Globals { get; set; }
+    public static Metadata Metadata { get; set; }
 }
