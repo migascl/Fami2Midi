d22bd9c7632bfbe34a2dc8960a4b0f56f7e2b5d9
Miguel Leirosa <miguelleirosa@gmail.com>
2024-09-20 15:32:30 +0100

    Changed Project properties modifiers

--- FamiTracker/Project.cs
+++ FamiTracker/Project.cs
@@ -2,8 +2,8 @@
 
 public class Project
 {
-    public static Globals Globals { get; set; }
-    public static Metadata Metadata { get; set; }
+    public Globals Globals { get; set; }
+    public Metadata Metadata { get; set; }
     public List<Instrument> Instruments { get; } = new List<Instrument>();
     public List<Song> Songs { get; } = new List<Song>();
 }
